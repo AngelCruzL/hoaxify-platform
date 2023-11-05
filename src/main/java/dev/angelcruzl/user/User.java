069fe7cf16e86726edda9d6389df14aafc7fb5a2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-11-05 00:00:12 -0600

    Add test case and functionality to save user into database

--- src/main/java/dev/angelcruzl/user/User.java
+++ src/main/java/dev/angelcruzl/user/User.java
@@ -1,9 +1,18 @@
 package dev.angelcruzl.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+  @Id
+  @GeneratedValue
+  private Long id;
   private String username;
   private String displayName;
   private String password;
--- src/test/java/dev/angelcruzl/UserControllerTest.java
+++ src/test/java/dev/angelcruzl/UserControllerTest.java
@@ -1,6 +1,8 @@
 package dev.angelcruzl;
 
 import dev.angelcruzl.user.User;
+import dev.angelcruzl.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +16,41 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class UserControllerTest {
+  private static final String API_1_0_USERS = "/api/1.0/users";
   @Autowired
   TestRestTemplate testRestTemplate;
 
-  @Test
-  public void postUser_whenUserIsValid_receiveOk() {
+  @Autowired
+  UserRepository userRepository;
+
+  @BeforeEach
+  public void cleanup() {
+    userRepository.deleteAll();
+  }
+
+  private User createValidUser() {
     User user = new User();
     user.setUsername("test-user");
     user.setDisplayName("test-display");
     user.setPassword("Secret.123");
 
-    ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
+    return user;
+  }
+
+  @Test
+  public void postUser_whenUserIsValid_receiveOk() {
+    User user = createValidUser();
+    ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
 
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
+
+
+  @Test
+  public void postUser_whenUserIsValid_userSavedToDatabase() {
+    User user = createValidUser();
+    testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+
+    assertThat(userRepository.count()).isEqualTo(1);
   }
 }
